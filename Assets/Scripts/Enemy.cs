2f36d5b6eb80249e8ece7f5a274cc016e83607c6
hw401 <2188598946@qq.com>
2025-06-26 10:38:37 +0800

    调整Player和TestEnemy预制件的缩放属性，更新PlayerController和EnemyController脚本以支持朝向更新逻辑

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,12 +16,12 @@ public enum ExpType
 public class Enemy : MonoBehaviour
 {
     #region 敌人属性
-    private float _maxHealth = 100f; // 最大生命值
-    private float _health = 100f; // 当前生命值
-    private float _damage = 1f; // 攻击力,接触玩家时造成的伤害
-    private float _moveSpeed = 5f; // 移动速度，作用于EnemyController
-    private float _mass = 1f; // 质量，影响敌人被击退的距离
-    private ExpType _expType = ExpType.Normal; // 经验类型
+    [SerializeField] private float _maxHealth = 100f; // 最大生命值
+    [SerializeField] private float _health = 100f; // 当前生命值
+    [SerializeField] private float _damage = 1f; // 攻击力,接触玩家时造成的伤害
+    [SerializeField] private float _moveSpeed = 1f; // 移动速度，作用于EnemyController
+    [SerializeField] private float _mass = 1f; // 质量，影响敌人被击退的距离
+    [SerializeField] private ExpType _expType = ExpType.Normal; // 经验类型
     #endregion
 
     private EnemyController _enemyController;
@@ -77,17 +77,5 @@ public class Enemy : MonoBehaviour
         {
             _health = 0;
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
