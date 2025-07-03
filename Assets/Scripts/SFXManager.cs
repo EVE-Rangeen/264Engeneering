22f894adb29d7aae91bc706ab8da2fae302abddc
Du Yifeng <a4r5t6y@yeah.net>
2025-07-03 23:14:10 +0800

    更改了SFX脚本 公开了新的音效的调用函数方法

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -11,17 +11,33 @@ public class SFXManager : MonoBehaviour
 {
     public static SFXManager instance;
 
+    [Header("音效索引")]
+    [Tooltip("冲刺音效")]
+    [SerializeField] private int _dashSFXIndex = 7;
+    [Tooltip("击中音效")]
+    [SerializeField] private int _hitSFXIndex = 8;
+    [Tooltip("被击中音效")]
+    [SerializeField] private int _hitBySFXIndex = 9;
+    [Tooltip("拾取音效")]
+    [SerializeField] private int _pickUpSFXIndex = 10;
+    [Tooltip("升级音效")]
+    [SerializeField] private int _levelUpSFXIndex = 11;
+    [Tooltip("使用血瓶音效")]
+    [SerializeField] private int _useHealthBottleSFXIndex = 12;
+    [Tooltip("点击按钮音效")]
+    [SerializeField] private int _clickButtonSFXIndex = 13;
+
     public AudioMixer _audioMixer;
     private void Awake()
     {
         instance = this;
     }
-    
+
     //这个数组要存储所有音效 然后在需要播放的函数里面调用PlaySFX或者PlaySFXPitched函数
     //例如 ： SFXManager.instance.PlaySFXPitched(6);表示播放数组里面的第六个音效
     //AudioSource组件中的Output放AudioMixer 音乐放Music 音效放SFX 分别由两个Mixer分别控制 这两个又由Master控制
     public AudioSource[] soundEffects;
-   
+
 
     //这个函数是用来播放音调不变的音效 在播放上一个相同音效的时候，会停止上一个音效然后继续播放这个音效
     //比如拾取音效当你连续拾取物品的时候 会先停止上一个拾取音效然后重新播放
@@ -55,5 +71,62 @@ public class SFXManager : MonoBehaviour
     public void SetSFXVolume(float volume)
     {
         _audioMixer.SetFloat("VolumeOfSFX", volume);
+    }
+
+    // 以下是播放各种音效的函数
+    /// <summary>
+    /// 播放冲刺音效
+    /// </summary>
+    public void PlayDashSFX()
+    {
+        PlaySFX(_dashSFXIndex);
+    }
+
+    /// <summary>
+    /// 播放击中音效
+    /// </summary>
+    public void PlayHitSFX()
+    {
+        PlaySFXPitched(_hitSFXIndex);
+    }
+
+    /// <summary>
+    /// 播放被击中音效
+    /// </summary>
+    public void PlayHitBySFX()
+    {
+        PlaySFXPitched(_hitBySFXIndex);
+    }
+
+    /// <summary>
+    /// 播放拾取音效
+    /// </summary>
+    public void PlayPickUpSFX()
+    {
+        PlaySFXPitched(_pickUpSFXIndex);
+    }
+
+    /// <summary>
+    /// 播放升级音效
+    /// </summary>
+    public void PlayLevelUpSFX()
+    {
+        PlaySFX(_levelUpSFXIndex);
+    }
+
+    /// <summary>
+    /// 播放使用血瓶音效
+    /// </summary>
+    public void PlayUseHealthBottleSFX()
+    {
+        PlaySFX(_useHealthBottleSFXIndex);
+    }
+
+    /// <summary>
+    /// 播放点击按钮音效
+    /// </summary>
+    public void PlayClickButtonSFX()
+    {
+        PlaySFX(_clickButtonSFXIndex);
     }
 }
